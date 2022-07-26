d12980531eb1aa86e506ae955415ef7b24b89bf1
AleksandrAstahov <spirtt1985@yandex.ru>
2022-07-26 14:19:42 +0300

    Push

--- src/main/java/ru/netology/ProductRepository.java
+++ src/main/java/ru/netology/ProductRepository.java
@@ -1,4 +1,31 @@
 package ru.netology;
 
+
 public class ProductRepository {
-}
+    Product[] products = new Product[0];
+
+    public void add (Product product) {
+        Product [] tmp = new Product[products.length + 1];
+
+        for (int i = 0; i < products.length; i++) {
+            tmp[i] = products[i];
+        }
+        tmp[tmp.length - 1] = product;
+        products = tmp;
+        }
+
+
+    public Product[] FindAll() {
+        return products;
+    }
+
+//    public Product[] DeleteById(int id) {
+//        Product[] result = new Product[0];
+//
+//        for (Product product : products) {
+//            if (products.matches(query)){
+//
+//            }
+//
+//        }
+    }
